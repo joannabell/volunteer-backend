ca479242da70a7e82f3e85c18e270c7dd7956f43
Joanna Bell <jjbdigital@gmail.com>
2022-11-14 14:14:30 -0600

    added more CRUD

--- app/controllers/signups_controller.rb
+++ app/controllers/signups_controller.rb
@@ -3,9 +3,26 @@ class SignupsController < ApplicationController
         render json: Signup.all, status: :ok
     end
 
+    def show 
+        signup = Signup.find(params[:id])
+        render json: signup, status: :ok 
+    end
+
     def create
         signup = Signup.create!(signup_params)
         render json: signup.activity, status: :created
+    end
+
+    def update
+        signup = Signup.find(params[:id])
+        signup.update(signup_params)
+        render json: signup, status: :accepted
+    end
+
+    def destroy
+        signup = Signup.find(params[:id])
+        signup.destroy 
+        render json: {}, status: :ok
     end
 
     private 
