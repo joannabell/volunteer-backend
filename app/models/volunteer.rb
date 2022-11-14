eab959014de28b648dfbb2feb18be1ec0c5b8828
Joanna Bell <jjbdigital@gmail.com>
2022-11-14 12:28:06 -0600

    added validations

--- app/models/volunteer.rb
+++ app/models/volunteer.rb
@@ -1,4 +1,8 @@
 class Volunteer < ApplicationRecord
     has_many :signups 
     has_many :activities, through: :signups 
+
+    validates :name, presence: true
+    validates :age, numericality: { greater_than_or_equal_to: 18 }
+    validates :email, presence: true
 end
